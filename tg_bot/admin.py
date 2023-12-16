4d0a99a333d3db2aa64ccf636a02f9b17cbe34f2
Weffy61 <weffy@mail.ru>
2023-12-16 16:15:26 +0300

    Add button to admin panel

--- tg_bot/admin.py
+++ tg_bot/admin.py
@@ -1,5 +1,11 @@
+import json
+
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import path
+
 from .models import *
+from .tasks import start_game_task
 
 
 @admin.register(UserSantaGame)
@@ -12,7 +18,6 @@ class UserSantaGameAdmin(admin.ModelAdmin):
         "id",
     )
     readonly_fields = ['is_game_start']
-
 
 
 @admin.register(Bonus)
@@ -34,4 +39,21 @@ class AssistantAdmin(admin.ModelAdmin):
 class GameAdmin(admin.ModelAdmin):
     list_display = ['info', 'end_game', 'promo_key', 'id']
     readonly_fields = ['promo_key', 'id']
+    change_list_template = 'game_change_list.html'
 
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('start_game/', self.draw_of_lots, name='draw_of_lots'),
+        ]
+        return custom_urls + urls
+
+    def draw_of_lots(self, request):
+        promos = [game.promo_key for game in
+                  Game.objects.all()]
+        promos_serialized = json.dumps(promos)
+        print(promos_serialized)
+        start_game_task.delay(promos_serialized)
+        self.model.objects.all().update(end_game=None)
+        self.message_user(request, 'Жребьевка успешно проведена')
+        return HttpResponseRedirect('../')
